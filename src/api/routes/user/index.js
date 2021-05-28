eb5867d524c5feea8364717aa6d4218a855cea18
Carlos <carlosoftwar@gmail.com>
2021-05-28 19:51:26 -0300

    feat : adicionando template para rotas

---
+++ src/api/routes/user/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { findAll, findOne, create, update, deleteAll, deleteOne } = require('../../../service/user');
+
+router.get('/', findAll);
+router.get('/:user_id', findOne);
+router.post('/', create);
+router.put('/', update);
+router.delete('/:user_id', deleteOne);
+router.delete('/', deleteAll);
+
+module.exports = router
