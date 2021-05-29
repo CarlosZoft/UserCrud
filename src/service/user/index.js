98b574c08b957f7c0e80c484e8d661144e187e26
Carlos <carlosoftwar@gmail.com>
2021-05-29 02:30:08 -0300

    update : adicionando find One

--- src/service/user/index.js
+++ src/service/user/index.js
@@ -11,10 +11,18 @@ exports.findAll = (req, res) => {
 
   res.status(200).send(file);
 }
-exports.findOne = () => {
+exports.findOne = (req, res) => {
+  const file = this.readFile();
+  const { user_id } = req.param;
+  const index = file.findIndex(user => user.id == user_id)
 
+  if (index != -1) {
+    res.status(200).send(file[index]);
 }
-exports.create = () => {
+  else {
+    res.status(404).send({ mensagemError: "Usuario não encontrado" });
+  }
+}
 
 }
 exports.update = () => {
