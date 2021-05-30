542c2420115e0a6f15ae737da331daf8a3408f6a
Carlos <carlosoftwar@gmail.com>
2021-05-30 13:11:51 -0300

    Update : create middleare for body validation

--- src/api/routes/user/index.js
+++ src/api/routes/user/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { findAll, findOne, create, update, deleteAll, deleteOne } = require('../../../service/user');
+const { Validator } = require('../../../validation/user');
 
 router.get('/', findAll);
 router.get('/:user_id', findOne);
-router.post('/', create);
-router.put('/:user_id', update);
+router.post('/', Validator, create);
+router.put('/:user_id', Validator, update);
 router.delete('/:user_id', deleteOne);
 router.delete('/', deleteAll);
 
--- src/shared/fileMethods.js
+++ src/shared/fileMethods.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = "./src/database/user.json"
 
-exports.readFile = (path) => {
+exports.readFile = () => {
   const file = fs.readFileSync(path, 'utf-8');
   return JSON.parse(file);
 }
-exports.writeFile = (path, file) => {
+exports.writeFile = (file) => {
   fs.writeFile(path, JSON.stringify(file), 'utf-8', (error) => {
     return error;
   })
