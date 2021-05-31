b1ea63062067e8d9c4b367728ac0da64fb50837d
Carlos <carlosoftwar@gmail.com>
2021-05-31 11:22:47 -0300

    fix : consertando put por senha

--- src/validation/user/index.js
+++ src/validation/user/index.js
@@ -13,16 +13,18 @@ exports.existsEmail = (email_current) => {
       "param": "email",
       "location": "body"
     }
-
   }
   else
     return false
 }
 exports.validateRequest = (req) => {
   const result = validationResult(req);
+  if (!req.body.email && !req.body.senha)
+    result.errors.push({ errorMessage: "informe os dados para atualizar" })
   return result.errors;
 };
-exports.Validator = [
+
+exports.validatorCreate = [
   check('email')
     .notEmpty()
     .withMessage('email não informado')
@@ -34,6 +36,19 @@ exports.Validator = [
   check('senha')
     .notEmpty()
     .withMessage('senha não informada')
+    .isLength({ min: 5 })
+    .withMessage('O campo senha deve conter pelo menos 5 caracteres')
+];
+exports.validatorUpdate = [
+  check('email')
+    .optional()
+    .isLength({ min: 9 })
+    .withMessage('O campo email deve conter pelo menos 9 caracteres')
+    .isEmail()
+    .withMessage('Precisa ser um e-mail válido')
+  ,
+  check('senha')
+    .optional()
     .isLength({ min: 5 })
     .withMessage('O campo senha deve conter pelo menos 5 caracteres')
 ];
